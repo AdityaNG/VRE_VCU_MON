c9868aedacd9a3f621d87000a7ddbfdd1c2b891e
AdityaNG <adityang5@gmail.com>
2021-05-23 20:06:40 +0530

    Current meter for main motor

--- control.py
+++ control.py
@@ -44,31 +44,91 @@ def send_command(inp):
             print("Connection Error : " + str(e))
             time.sleep(1)
 
+
+global win
 prev_cmd = ''
 inp=1
 throttle=0
 steering=0
-MAX_TH = 60
+MAX_TH = 100
 MAX_ST = 100
 creep_mode = 0
 width = 500
 win = pygame.display.set_mode((width, width))
+data = {}
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+green = (0, 255, 0)
+blue = (0, 0, 128)
+
+MAX_CURRENT_L = 0
+MAX_CURRENT_R = 0
 
 pygame.joystick.init()
 joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+
+def get_data(key):
+    if key not in data:
+        return 0.0
+    return data[key]
+
+def draw_win():
+    global win, MAX_CURRENT_L, MAX_CURRENT_R
+    
+    cur_l = get_data('CURRENT_DRAW_L')
+
+    if cur_l > MAX_CURRENT_L:
+        MAX_CURRENT_L = cur_l
+        print("MAX_CURRENT_L : ", MAX_CURRENT_L)
+    
+    cur_r = get_data('CURRENT_DRAW_R')
+
+    if cur_r > MAX_CURRENT_R:
+        MAX_CURRENT_R = cur_r
+        print("MAX_CURRENT_R : ", MAX_CURRENT_R)
+
+    font = pygame.font.Font('freesansbold.ttf', 12)
+
+    current_l_text = font.render('CURRENT_DRAW_L : ' + str(int(cur_l)), True, green, blue)
+    current_r_text = font.render('CURRENT_DRAW_R : ' + str(int(cur_r)), True, green, blue)
+    
+    current_l_textRect = current_l_text.get_rect()
+    current_r_textRect = current_r_text.get_rect()
+    current_l_textRect.center = (width//2, 70)
+    current_r_textRect.center = (width//2, 170)
+
+    win.fill( black )
+    win.blit(current_l_text, current_l_textRect)
+    win.blit(current_r_text, current_r_textRect)
+    
+    min_cutoff = 1300
+    cur_l_w = cur_l
+    if cur_l > min_cutoff:
+        cur_l_w -= min_cutoff
+    
+    cur_r_w = cur_r
+    if cur_r > min_cutoff:
+        cur_r_w -= min_cutoff
+
+    pygame.draw.rect(win, ( int(255 * cur_l/4095), int(255 * (1 - cur_l/4095) ) , 0) , pygame.Rect(0, 0, int(width * cur_l_w/(4095-min_cutoff)), 50))
+    pygame.draw.rect(win, ( int(255 * cur_r/4095), int(255 * (1 - cur_r/4095) ) , 0) , pygame.Rect(0, 100, int(width * cur_r_w/(4095-min_cutoff)), 50))
+    pygame.display.flip()
+    
+    pygame.display.update()
+
 
 print(joysticks)
 
 while 1 :
     for event in pygame.event.get():
-        print(event)
+        #print(event)
         if event.type == pygame.QUIT:
             ser.close()
             pygame.quit() #sys.exit() if sys is imported
             exit()
         if event.type == pygame.JOYAXISMOTION:
             j = event.joy, event.axis, event.value
-            print(j)
             if event.axis == 5: # Break
                 throttle = int((event.value+1) /2 * -MAX_TH)
                 creep_mode = 0
@@ -85,7 +145,6 @@ while 1 :
                 pass
         if event.type == pygame.JOYBUTTONDOWN:
             j = event.joy, event.button
-            print(j)
             if event.button == 11: # Start
                 inp = "start;"
                 send_command(inp)
@@ -147,11 +206,11 @@ while 1 :
         
         # send the character to the device
         # (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
-        if inp != prev_cmd:
+        if inp != prev_cmd or True:
             send_command(inp)
             #ser.write(str(inp + '\r').encode('utf-8'))
             prev_cmd = inp
-            print(">> " + inp)
+            #print(">> " + inp)
         #out = ''
         # let's wait one second before reading output (let's give device time to answer)
         #time.sleep(1)
@@ -162,6 +221,26 @@ while 1 :
         #    print(">>" + out)
     #time.sleep(0.1)
     pygame.time.delay(200)
+    out = ""
+    while ser.inWaiting() > 0:
+        out += ser.read(1).decode('utf-8')
+
+    if out != '':
+        data = {}
+        for ele in out.split(","):
+            if ':' in ele:
+                try:
+                    key, val = ele.split(":")
+                    key = key.strip()
+                    val = float(val)
+                    data[key] = val
+                except Exception as e:
+                    print("Err ele : " + ele)
+        #print(data)
+        draw_win()
+        # {'ax': 1788.0, ' ay': 512.0, ' az': -15148.0, ' gx': 263.0, ' gy': -212.0, ' gz': 183.0, ' CURRENT_DRAW_L': 1616.0, ' CURRENT_DRAW_R': 1918.0, ' throttle_val': 1.0, ' steering_val': 0.0, ' RTC_TIME': 0.0}
+
+
     if inp=='start;' or event=='stop;':
         inp = 1
 
